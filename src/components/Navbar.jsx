0edc5b36ac4c8ff2973d40f6594d0ee354e2c68b
flux-hjkim <hecho3927@gmail.com>
2026-07-03 17:25:53 +0900

    style: restructure navbar menu layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const navItems = [
+  { label: "HOME", path: "/" },
+  { label: "MYPALETTE", path: "/mypalette" },
+  { label: "PIECES OF ME", path: "#" },
+  { label: "PALETTE LOG", path: "#" },
+];
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Your Color Palette</Link>
-      <Link to="/mypalette">My Palette</Link>
+    <nav className="navbar">
+      <div className="navbar-menu">
+        {navItems.map((item, index) => (
+          <div className="navbar-item-wrap" key={item.label}>
+            <NavLink
+              to={item.path}
+              className={({ isActive }) =>
+                item.path !== "#" && isActive
+                  ? "navbar-link active"
+                  : "navbar-link"
+              }
+            >
+              {item.label}
+            </NavLink>
+
+            {index < navItems.length - 1 && (
+              <span className="navbar-divider">/</span>
+            )}
+          </div>
+        ))}
+      </div>
     </nav>
   );
 }
